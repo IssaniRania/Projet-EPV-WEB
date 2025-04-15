e5e1d48b425d0665ab60441d5f7c3b703b441db3
Rania Issani <raniaissani83@gmail.com>
2025-04-16 00:13:20 +0100

    mot de passe oublié

--- EpvWeb-rania/src/sections/auth/Forgot-Password-View.tsx
+++ EpvWeb-rania/src/sections/auth/Forgot-Password-View.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useState ,useCallback,useRef } from 'react';
 import { TextField, Button, Typography, Box, Alert } from '@mui/material';
 import axios from 'axios';
+import { useRouter } from 'src/routes/hooks';
+import IconButton from '@mui/material/IconButton';
+import { Iconify } from 'src/components/iconify';
+import InputAdornment from '@mui/material/InputAdornment';
+
 
 export  function ForgotPasswordView() {
+  const router = useRouter();
+
   const [code, setCode] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [message, setMessage] = useState('');
   const [error, setError] = useState('');
-
+  const [showPassword, setShowPassword] = useState(false);
+   const passwordRef = useRef<HTMLInputElement>(null);
   const handleReset = async () => {
     try {
       const response = await axios.post('http://localhost:5088/api/auth/reset-password', {
@@ -15,10 +23,25 @@ export  function ForgotPasswordView() {
         newPassword
       });
       setMessage(response.data.message);
+      router.push('/sign-in');
       setError('');
     } catch (err: any) {
       setMessage('');
       setError(err.response?.data?.message || 'Erreur lors de la réinitialisation.');
+    }
+  };
+const handleRetour = useCallback(() => {
+    router.push('/sign-in');
+  }, [router]);
+  const handleCodeKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter' && passwordRef.current) {
+      passwordRef.current.focus(); // va au champ mot de passe
+    }
+  };
+
+  const handlePasswordKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      handleReset(); // déclenche le bouton
     }
   };
 
@@ -31,24 +54,61 @@ export  function ForgotPasswordView() {
       {message && <Alert severity="success">{message}</Alert>}
       {error && <Alert severity="error">{error}</Alert>}
 
-      <TextField
+      {/* <TextField
         fullWidth
         label="Code utilisateur"
         value={code}
         onChange={(e) => setCode(e.target.value)}
         sx={{ mt: 2 }}
-      />
-      <TextField
+      /> */}
+      {/* <TextField
         fullWidth
         label="Nouveau mot de passe"
         type="password"
         value={newPassword}
         onChange={(e) => setNewPassword(e.target.value)}
         sx={{ mt: 2 }}
+      /> */}
+<TextField
+        fullWidth
+        name="Code"
+        label="Code"
+        value={code}
+  onChange={(e) => setCode(e.target.value)}
+  onKeyDown={handleCodeKeyDown}
+  InputLabelProps={{ shrink: true }}
+        sx={{ mb: 3 }}
       />
-      <Button variant="contained" fullWidth onClick={handleReset} sx={{ mt: 3 }}>
-        Réinitialiser
-      </Button>
+<TextField
+        fullWidth
+        name="password"
+        label="Password"
+        value={newPassword}
+  onChange={(e) => setNewPassword(e.target.value)}
+  onKeyDown={handlePasswordKeyDown}
+  inputRef={passwordRef}
+  InputLabelProps={{ shrink: true }}
+        type={showPassword ? 'text' : 'password'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                <Iconify icon={showPassword ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        sx={{ mt: 2 }}
+      />
+ <Box sx={{ display: 'flex', gap: 2, mt: 3 }}>
+  <Button variant="contained" fullWidth onClick={handleReset}>
+    Réinitialiser
+  </Button>
+  <Button variant="contained" fullWidth onClick={handleRetour}>
+    Annuler
+  </Button>
+</Box>
+
     </Box>
   );
 }
